7ca7e1b965426913a31489a5ac1dda3aa0e9e30d
widipa <widipa@gmail.com>
2016-09-21 20:22:15 -0500

    Ajuste en uso de logger

--- siatur/siatur-web/src/main/java/co/edu/uan/app/siatur/util/FacesUtils.java
+++ siatur/siatur-web/src/main/java/co/edu/uan/app/siatur/util/FacesUtils.java
@@ -1,21 +1,19 @@
 package co.edu.uan.app.siatur.util;
 
 import java.security.Principal;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.faces.context.FacesContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FacesUtils {
 
-    private static final Logger logger = Logger.getLogger(FacesUtils.class.toString());
+    private static final Logger logger = LoggerFactory.getLogger(FacesUtils.class);
 
 	
     public static Principal getPrincipal() {
 
-    	logger.log(Level.INFO, "FacesContext.getCurrentInstance() = "+FacesContext.getCurrentInstance());
-    	logger.log(Level.INFO, "FacesContext.getCurrentInstance().getExternalContext() = "+FacesContext.getCurrentInstance().getExternalContext());
-    	logger.log(Level.INFO, "FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal() = "+FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal());
         return FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
     }
 	
